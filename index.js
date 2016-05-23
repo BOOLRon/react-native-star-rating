bd3f61a3b1f05b15ab0708db86083ad75c9be1b8
BOOLRon <ron.zhang.36@gmail.com>
2016-05-23 15:24:04 +0800

    fix syntax error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import ReactNative, {
   StyleSheet,
   View
 } from 'react-native';
-import React {Component,PropTypes} from 'react';
+import React, {Component,PropTypes} from 'react';
 // Third party imports
 import Button from 'react-native-button';
 import EntypoIcons from 'react-native-vector-icons/Entypo';
